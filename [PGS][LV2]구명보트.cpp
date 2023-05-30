7c8e588e3de89dbc2a541bfabbff09fb449f7b7b
ha <areumh01@gmail.com>
2023-05-30 13:50:51 +0900

    투포인터 사용(정답)

--- [PGS][LV2]구명보트.cpp
+++ [PGS][LV2]구명보트.cpp
@@ -5,33 +5,27 @@
 
 using namespace std;
 
-bool visited[50002];
-
 int solution(vector<int> people, int limit) {
     int boat = 0;
+
+    int left = 0;
+    int right = people.size() - 1;
     sort(people.begin(), people.end());
-    for (int i = people.size() - 1; i >= 0; i--) {
-        bool flag = false;
-        if (visited[i] == true) continue;
-        if ((limit - people[i]) < 40) {
+    while (left <= right) {
+        if (people[left] + people[right] > limit) {
+            right--;
             boat++;
-            visited[i] = true;
-            //cout << people[i] << "Å¾½Â" << '\n';
-            continue;
         }
         else {
-            for (int j = 0; j < people.size(); j++) {
-                if (visited[j] == true) continue;
-                if (people[j] <= (limit - people[i])) {
-                    visited[j] = true;
-                    boat++;
-                    //cout << people[i] << "¶û °°ÀÌ Å¾½ÂÇÑ" << people[j] << '\n';
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag == false) boat++;
+            left++;
+            right--;
+            boat++;
+        }
+        if (right == left) {
+            boat++;
+            break;
         }
     }
+
     return boat;
 }
