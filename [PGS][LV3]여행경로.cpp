5c3dbb6e15329b8f236fdf3185bff48cbb1b8004
areumh01 <areumh01@gmail.com>
2023-06-03 14:36:58 +0900

    다시 저장함 DFS

--- [PGS][LV3]여행경로.cpp
+++ [PGS][LV3]여행경로.cpp
@@ -1,43 +1,34 @@
-#include <string.h>
+#include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int result = 0;
-bool visited[102];
-vector<vector<pair<int, int>>> graph;
+vector<string> answer;
+vector<int> visited;
 
-void prim() {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({ 0,0 });
-
-    while (!pq.empty()) {
-        int here = pq.top().second;
-        int hereDist = pq.top().first;
-        pq.pop();
-        if (visited[here]) continue;
-        visited[here] = true;
-        result += hereDist;
-
-        for (int i = 0; i < graph[here].size(); i++) {
-            int there = graph[here][i].second;
-            int thereDist = graph[here][i].first;
-            if (visited[there]) continue;
-            pq.push({ thereDist,there });
+void dfs(vector<vector<string>>& tickets, string start, int count) {
+    if (count == tickets.size() + 1) {
+        return;
+    }
+    for (int i = 0; i < tickets.size(); i++) {
+        if (tickets[i][0] == start && visited[i] == 0) {
+            cout << tickets[i][0] << " ";
+            visited[i] = 1;
+            answer.push_back(tickets[i][1]);
+            dfs(tickets, tickets[i][1], count + 1);
+            if (answer.size() == tickets.size() + 1) return;
+            visited[i] = 0;
+            answer.pop_back();
         }
     }
 }
 
-int solution(int n, vector<vector<int>> costs) {
-    graph.resize(n + 2);
-    for (int i = 0; i < costs.size(); i++) {
-        graph[costs[i][1]].push_back({ costs[i][2],costs[i][0] });
-        graph[costs[i][0]].push_back({ costs[i][2],costs[i][1] });
-    }
-    prim();
-
-    return result;
+vector<string> solution(vector<vector<string>> tickets) {
+    sort(tickets.begin(), tickets.end());
+    visited.resize(tickets.size() + 2);
+    answer.push_back("ICN");
+    dfs(tickets, "ICN", 0);
+    return answer;
 }
