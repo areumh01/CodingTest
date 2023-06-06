789be1d6d955e99d812bafb14be80cbae3b66c2e
areumh01 <areumh01@gmail.com>
2023-06-06 21:17:44 +0900

    효율성 훨씬 높은 풀이

--- [PGS][LV2]귤 고르기.cpp
+++ [PGS][LV2]귤 고르기.cpp
@@ -7,35 +7,18 @@ using namespace std;
 
 int solution(int k, vector<int> tangerine) {
     int answer = 0;
-    int start = 1;
-    int end = 10000000;
-    vector<int> tan;
-    tan.resize(10000002);
+    int m = *max_element(tangerine.begin(), tangerine.end());
+    vector<int> tan(m,0);
 
     for (int i = 0; i < tangerine.size(); i++) {
-        tan[tangerine[i]]++;
+        tan[tangerine[i]-1]++;
     }
     sort(tan.begin(), tan.end(), greater<>());
 
-    while (start <= end) {
-        int counting = 0;
-        int mid = (start + end) / 2;
-        for (int i = 0; i < tan.size(); i++) {
-            if (mid <= tan[i]) {
-                counting += tan[i];
-            }
-        }
-        if (counting < k) end = mid - 1;
-        else start = mid + 1;
-    }
-
-    int sub = 0;
     for (int i = 0; i < tan.size(); i++) {
-        if (tan[i] >= end) {
-            sub += tan[i];
-            answer++;
-        }
-        if (sub >= k) break;
+        answer++;
+        k -= tan[i];
+        if (k <= 0) break;
     }
 
     return answer;
