facb80e10b12941660f91a32b10e376ac0f7cfec
areumh01 <areumh01@gmail.com>
2023-06-11 20:01:20 +0900

    이분탐색

--- [PGS][LV3]입국심사.cpp
+++ [PGS][LV3]입국심사.cpp
@@ -1,34 +1,22 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-vector<string> answer;
-vector<int> visited;
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    long long start = 0;
+    long long end = 1e18;
+    long long mid = 0;
 
-void dfs(vector<vector<string>>& tickets, string start, int count) {
-    if (count == tickets.size() + 1) {
-        return;
-    }
-    for (int i = 0; i < tickets.size(); i++) {
-        if (tickets[i][0] == start && visited[i] == 0) {
-            cout << tickets[i][0] << " ";
-            visited[i] = 1;
-            answer.push_back(tickets[i][1]);
-            dfs(tickets, tickets[i][1], count + 1);
-            if (answer.size() == tickets.size() + 1) return;
-            visited[i] = 0;
-            answer.pop_back();
+    while (start <= end) {
+        mid = (start + end) / 2;
+        long long counting = 0;
+        for (int i = 0; i < times.size(); i++) {
+            counting += mid / times[i];
         }
+        if (counting >= n) end = mid - 1;
+        else start = mid + 1;
     }
-}
-
-vector<string> solution(vector<vector<string>> tickets) {
-    sort(tickets.begin(), tickets.end());
-    visited.resize(tickets.size() + 2);
-    answer.push_back("ICN");
-    dfs(tickets, "ICN", 0);
-    return answer;
+    return start;
 }
