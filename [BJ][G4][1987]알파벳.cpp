d0b0a617f5b8ebf6f9001168d19c0d5da8f5ebaa
areumh01 <areumh01@gmail.com>
2023-08-21 21:42:31 +0900

    시간단축ver DFS

--- [BJ][G4][1987]알파벳.cpp
+++ [BJ][G4][1987]알파벳.cpp
@@ -11,43 +11,37 @@ using namespace std;
 
 int n, m;
 int answer = 0;
+int counting = 0;
 vector<string> map(22);
 int x[4] = { -1,1,0,0 };
 int y[4] = { 0,0,-1,1 };
 bool visited[22][22];
 bool alpha[28];
 
-void dfs(int cx, int cy, int counting) {
-	bool check = false;
-	for (int i = 0; i < 4; i++) {
-		int nextx = cx + x[i];
-		int nexty = cy + y[i];
+void dfs(pair<int, int> a) {
+	alpha[map[a.first][a.second] - 'A'] = true;
+	counting++;
 
-		if (nextx >= 0 && nextx < n && nexty >= 0 && nexty < m && !visited[nextx][nexty] && !alpha[map[nextx][nexty] - 'A']) {
-			alpha[map[nextx][nexty] - 'A'] = true;
-			visited[nextx][nexty] = true;
-			dfs(nextx, nexty, counting + 1);
-			alpha[map[nextx][nexty] - 'A'] = false;
-			visited[nextx][nexty] = false;
-			check = true;
+	for (int i = 0; i < 4; i++) {
+		int nextx = a.first + x[i];
+		int nexty = a.second + y[i];
+		if (nextx >= 0 && nextx < n && nexty >= 0 && nexty < m && !alpha[map[nextx][nexty] - 'A']) {
+			dfs({ nextx,nexty });
 		}
 	}
-	if (!check) {
-		answer = max(answer, counting);
-		return;
-	}
+	//빠져나오면 더이상 갈 방법이 없다는 의미
+	answer = max(counting, answer);
+	counting--;
+	alpha[map[a.first][a.second] - 'A'] = false;
 }
 
 int main() {
 	cin >> n >> m;
-	memset(visited, false, sizeof(visited));
 	memset(alpha, false, sizeof(alpha));
 
 	for (int i = 0; i < n; i++) {
 		cin >> map[i];
 	}
-	visited[0][0] = true;
-	alpha[map[0][0] - 'A'] = true;
-	dfs(0, 0, 1);
+	dfs({ 0,0 });
 	cout << answer << '\n';
 }
