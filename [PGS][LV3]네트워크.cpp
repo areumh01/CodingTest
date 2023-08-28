c4c72856a9ab559ca64a48d5f50d1ae1cb6b0b82
areumh01 <areumh01@gmail.com>
2023-08-28 12:47:24 +0900

    UNION-FIND ver

--- [PGS][LV3]네트워크.cpp
+++ [PGS][LV3]네트워크.cpp
@@ -1,37 +1,60 @@
 #include <string>
-#include <vector>
-#include <queue>
+#include <map>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class disj {
+private:
+    map<int, pair<int, int>> m;
+
+public:
+    disj(int n) {
+        for (int i = 0; i < n; i++) {
+            m[i + 1].first = i + 1;
+            m[i + 1].second = 0;
+        }
+    }
+    int find(int n) {
+        if (m[n].first == n)
+            return n;
+        else {
+            return find(m[n].first);
+        }
+    }
+    void Union(int a, int b) {
+        int roota = find(a);
+        int rootb = find(b);
+        if (roota == rootb) {
+            return;
+        }
+        else {
+            if (m[roota].second > m[rootb].second) m[rootb].first = roota;
+            else if (m[roota].second < m[rootb].second) m[roota].first = rootb;
+            else if (m[roota].second == m[rootb].second) {
+                m[rootb].first = roota;
+                m[roota].second++;
+            }
+        }
+    }
+};
+
 int solution(int n, vector<vector<int>> computers) {
+    disj dis(n);
+    int visited[202] = { 0, };
     int answer = 0;
-    vector<vector<int>> graph(n);
-    vector<bool> visited(n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j) continue;
-            if (computers[i][j] == 1)
-                graph[i].push_back(j);
+    for (int i = 0; i < computers.size(); i++) {
+        for (int j = 0; j < computers[i].size(); j++) {
+            if (j != i && computers[i][j] == 1) {
+                dis.Union(i + 1, j + 1);
+            }
         }
     }
     for (int i = 0; i < n; i++) {
-        if (!visited[i]) {
+        if (visited[dis.find(i + 1)] == 0) {
+            visited[dis.find(i + 1)] = 1;
             answer++;
-            queue<int> q;
-            q.push(i);
-            visited[i] = true;
-            while (!q.empty()) {
-                int current = q.front();
-                q.pop();
-                for (int j = 0; j < graph[current].size(); j++) {
-                    if (!visited[graph[current][j]]) {
-                        visited[graph[current][j]] = true;
-                        q.push(graph[current][j]);
-                    }
-                }
-            }
         }
     }
     return answer;
