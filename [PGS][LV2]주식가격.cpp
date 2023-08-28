4ee475a7bb0a2c1ff96061c5c76c9990976c7d39
areumh01 <areumh01@gmail.com>
2023-08-28 16:00:50 +0900

    스택 - 재풀이

--- [PGS][LV2]주식가격.cpp
+++ [PGS][LV2]주식가격.cpp
@@ -1,36 +1,26 @@
 #include <string>
 #include <vector>
 #include <stack>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer(prices.size(), -1);
+    vector<int> answer(prices.size());
     stack<pair<int, int>> s;
-    s.push({ prices[0],0 });
-    int index = 1;
-
-    while (!s.empty() && index != prices.size()) {
-        if (prices[index] >= s.top().first) {
-            s.push({ prices[index],index });
-        }
-        else {
-            while (!s.empty() && s.top().first > prices[index]) {
-                answer[s.top().second] = index - s.top().second;
-                s.pop();
-            }
-            s.push({ prices[index],index });
-        }
-        index++;
-    }
-
     for (int i = 0; i < prices.size(); i++) {
-        if (answer[i] == -1) {
-            if (s.empty()) answer[i] = prices.size() - i;
-            else answer[i] = s.top().second - i;
+        if (!s.empty() && s.top().first > prices[i]) {
+            while (!s.empty() && s.top().first > prices[i]) {
+                int index = s.top().second;
+                s.pop();
+                answer[index] = i - index;
+            }
         }
+        s.push({ prices[i],i });
     }
-
+    while (!s.empty()) {
+        int index = s.top().second;
+        s.pop();
+        answer[index] = prices.size() - 1 - index;
+    }
     return answer;
 }
